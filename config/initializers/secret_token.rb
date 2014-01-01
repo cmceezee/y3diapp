987836164ab86417044eac27e6566f4c045bad30
Chris D. McCoy <chrisdmccoy@gmail.com>
2014-01-01 13:52:28 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Y3diapp::Application.config.secret_key_base = '624145efce03bb17b034911167edf9977dc264fbfdcc6e1e568ba61ae0dd2ccbf71295372dafc0b8c7c4f01d3af20923107da5416b2b7f5d2a99619fd0bf91bf'
